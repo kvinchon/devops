d60447012bd002c57fe462795ea0a6da8816cd77
Kevin Vinchon <kevin.vinchon@gmail.com>
2018-12-01 16:54:40 +0100

    Initializing git repository

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"wsf/devops/handler"
+)
+
+func main() {
+
+	// connection := os.Getenv("DATABASE_URL")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	myHandler := handler.NewHandler()
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: myHandler,
+		// ReadTimeout:  10 * time.Second,
+		// WriteTimeout: 10 * time.Second,
+	}
+
+	fmt.Println("Everything is OK")
+	fmt.Println("Server is running at", port)
+
+	log.Fatal(server.ListenAndServe())
+}
